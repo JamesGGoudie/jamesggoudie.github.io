168298a270abda38b253bdd9cf498d5d210d301c
James Gabriel Goudie <james.g.goudie@gmail.com>
2022-01-27 18:30:27 -0500

    wordle helper

---
+++ static/js/53.55bb1135.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoudie-ca-src"]=this["webpackJsonpgoudie-ca-src"]||[]).push([[53],{154:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var c=e(16),i=e(13),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(c.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(n){return Object(i.c)(r,n)})).then((function(){Object(c.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.55bb1135.chunk.js.map
